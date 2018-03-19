7ae0805bc0acf085f0c7abd18b165b45320dc3d8
victordelval <victor.val.andres@gmail.com>
2018-03-19 16:27:50 +0100

    Adds POST /notes, route to create a new note

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -4,9 +4,16 @@ import mocks from '../mocks'
 
 const router = express.Router()
 
-router.get('/', (req, res, next) => {
-    res.status(200)
-        .json(mocks)
-})
+router
+    .get('/', (req, res, next) => {
+        // TODO - Read all notes from DB
+        res.status(200)
+            .json(mocks)
+    })
+    .post('/', (req, res, next) => {
+        // TODO - Create a new note and save into DB
+        res.status(201)
+            .json(req.body)
+    })
 
 export default router
