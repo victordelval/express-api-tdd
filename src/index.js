db2e567523f707254cf5a9b481292a527d2b79c3
victordelval <victor.val.andres@gmail.com>
2018-03-19 11:43:49 +0100

    Adds core dependency (express), with dummy server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('Hello world!')
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.end('Hello world!!! :-P')
+})
+
+app.listen('9000', () => {
+    console.log('Server listening at http://localhost:9000')
+})
