f2c1d77aab3c6ec8de89d2a1acbaeb38c5e2df6e
victordelval <victor.val.andres@gmail.com>
2018-03-19 12:39:32 +0100

    Adds a wrapper for the server to manage the development and testing instances

--- src/index.js
+++ src/index.js
@@ -4,20 +4,38 @@ import config from './config'
 
 let _server
 
-const app = express()
+const server = {
 
-config(app)
+    start() {
+        const app = express()
+        config(app)
 
-app.get('/', (req, res, next) => {
-    res.end('Hello "configured" world!!!')
-})
+        app.get('/', (req, res, next) => {
+            res.end('Hello "configured" world!!!')
+        })
 
-_server = app.listen('9000', () => {
-    const address = _server.address()
-    const host = address.address === '::'
-        ? 'localhost'
-        : address
-    const port = app.locals.config.PORT
+        _server = app.listen('9000', () => {
+            const address = _server.address()
+            const host = address.address === '::'
+                ? 'localhost'
+                : address
+            const port = app.locals.config.PORT
 
-    console.log(`Server listening at http://${host}:${port}`)
-})
+            if (process.env.NODE_ENV !== 'test') {
+                console.log(`Server listening at http://${host}:${port}`)
+            }
+        })
+
+        return _server
+    },
+
+    close() {
+        _server.close()
+    }
+}
+
+export default server
+
+if (!module.parent) {
+    server.start()
+}
